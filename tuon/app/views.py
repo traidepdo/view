0bcb7ea07420640078e0004e033710ca1211e29e
Dat Pham <phamthanhdat4940@gmail.com>
2026-01-21 12:46:10 +0700

    fff

--- tuon/app/views.py
+++ tuon/app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    data = 'hello cc'
+    data = 'hello silon'
     return render(request, 'app/home.html', {
         'data': data
     })

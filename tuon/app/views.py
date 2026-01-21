2b526beeb96755fc49bced6fb2b843bf3d1363e0
traidepdo <nguyenvanngoan07072004@gmail.com>
2026-01-21 12:51:16 +0700

    ada

--- tuon/app/views.py
+++ tuon/app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    data = 'hello silon'
+    data = 'hello x2'
     return render(request, 'app/home.html', {
         'data': data
     })
